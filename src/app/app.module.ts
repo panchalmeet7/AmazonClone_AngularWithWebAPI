e1a261a5aaba207f9f4b8a6523bfbe424e5e1f40
meet <meetpanchal194@gmail.com>
2023-09-04 18:19:20 +0530

    Revert "product page completed (with list and grid view)"
    This reverts commit 4dd5483d9fa0de0cab5bb6cd19eb194fe8cfbbf8.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SuggestedProductsComponent } from './components/suggested-products/suggested-products.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    NavbarComponent,
     SuggestedProductsComponent,
-    HomeComponent,
-    ProductsComponent,
-    ProductDetailsComponent,
+    HomeComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
